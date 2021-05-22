f8e8285a7c1e18e0caadea01b941842f47554e95
Steven Abaco <stevenabaco@gmail.com>
2021-05-22 08:48:25 -0500

    Destructure Counter and Player props

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,25 @@
 import React from 'react';
 
-const Counter = props =>  {
-
-  let index = props.index;
-		return (
-			<div className='counter'>
-				<button
-					className='counter-action decrement'
-					onClick={() => props.changeScore(index, -1)}
-				>
-					{' '}
-					-{' '}
-				</button>
-				<span className='counter-score'>{props.score}</span>
-				<button
-					className='counter-action increment'
-					onClick={() => props.changeScore(index, 1)}
-				>
-					{' '}
-					+{' '}
-				</button>
-			</div>
-		);
-}
-
+const Counter = ({ index, score, changeScore }) => {
+	return (
+		<div className='counter'>
+			<button
+				className='counter-action decrement'
+				onClick={() => changeScore(index, -1)}
+			>
+				{' '}
+				-{' '}
+			</button>
+			<span className='counter-score'>{score}</span>
+			<button
+				className='counter-action increment'
+				onClick={() => changeScore(index, 1)}
+			>
+				{' '}
+				+{' '}
+			</button>
+		</div>
+	);
+};
 
 export default Counter;
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,30 +1,22 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import Counter from './Counter';
 
 class Player extends PureComponent {
-  render() {
-  console.log(this.props.name + ' rendered');
-	return (
-		<div className='player'>
-			<span className='player-name'>
-				<button
-					className='remove-player'
-					onClick={() => this.props.removePlayer(this.props.id)}
-				>
-					✖
-				</button>
-				{this.props.name}
-			</span>
+	render() {
+		const { name, id, score, index, removePlayer, changeScore } = this.props;
+		return (
+			<div className='player'>
+				<span className='player-name'>
+					<button className='remove-player' onClick={() => removePlayer(id)}>
+						✖
+					</button>
+					{name}
+				</span>
 
-			<Counter
-				score={this.props.score}
-				changeScore={this.props.changeScore}
-				index={this.props.index}
-			/>
-		</div>
-	);
-  }
-  
-};
+				<Counter score={score} changeScore={changeScore} index={index} />
+			</div>
+		);
+	}
+}
 
 export default Player;
